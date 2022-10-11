5aab2a3d1fe4684cbee246c97cdcfea1a79ed90a
Drummer143 <berg.evg.1@gmail.com>
2022-10-11 15:04:52 +0300

    created form for updating profile in settings

--- src/components/settingsPart/ProfileSettings/ProfileSettings.tsx
+++ src/components/settingsPart/ProfileSettings/ProfileSettings.tsx
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 import { toast, ToastOptions } from 'react-toastify';
+import React, { useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-import React, { useState } from 'react';
-import { AppState } from '../../../types/types';
+import { AppState, DynamicObject } from '../../../types/types';
 import { EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 import { emailSchema, passwordSchema, confirmPasswordSchema } from '../../../utils';
 import {
@@ -14,22 +14,25 @@ import {
 } from '../../../redux/actions/actions';
 
 import styles from './ProfileSettings.module.css';
+import Modal from './../../Modal/Modal';
+import UpdateProfileForm from '../UpdateProfileForm/UpdateProfileForm';
+import { auth } from './../../../firebase';
+
+type UpdatingField = {
+    field: string;
+    value: string;
+};
 
 const toastParams: ToastOptions = {
     position: 'top-right',
     autoClose: 5000,
-    hideProgressBar: true,
+    hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: false,
     progress: undefined,
     theme: 'colored'
 };
-
-type updatingField = {
-    field: string
-    value: string
-}
 
 function ProfileSettings() {
     const dispatch = useDispatch();
@@ -40,16 +43,19 @@ function ProfileSettings() {
         password: passwordSchema,
         confirmPassword: confirmPasswordSchema
     });
-    const [reAuthFormDisplay, setReAuthFormDisplay] = useState('none');
-    const [updatingField, setUpdatingField] = useState({} as updatingField);
-    const [password, setPassword] = useState('');
+    const [shouldShowModal, setShouldShowModal] = useState(false);
+    const [updatingField, setUpdatingField] = useState({} as UpdatingField);
+    const [password, setPassword] = useState('f');
+    const ModalInputRef = useRef<HTMLInputElement>(null);
 
     const reAuthentication = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setPassword('');
+        setUpdatingField({} as UpdatingField);
+
         const credential = EmailAuthProvider.credential(user.email || '', password);
-        reauthenticateWithCredential(user, credential)
-            .then(() => setReAuthFormDisplay('none'))
+        reauthenticateWithCredential(auth.currentUser || user, credential)
+            .then(() => setShouldShowModal(false))
             .then(() => {
                 switch (updatingField.field) {
                     case 'email':
@@ -64,118 +70,75 @@ function ProfileSettings() {
             .catch(error => console.error(error));
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>, field: string, value: string) => {
-        e.preventDefault();
-        if (value) {
-            switch (field) {
-                case 'name':
-                    if (user.displayName !== value) {
-                        dispatch(updateNameRequest(value));
-                    } else {
-                        toast.error('This name is already in use', toastParams);
-                    }
-                    break;
-
-                case 'email':
-                    if (user.email !== value) {
-                        setReAuthFormDisplay('flex');
-                        setUpdatingField({ field: 'email', value });
-                    } else {
-                        toast.error('This email is already in use', toastParams);
-                    }
-                    break;
-
-                case 'password':
-                    setReAuthFormDisplay('flex');
-                    setUpdatingField({ field: 'password', value });
-                    break;
-
-                default:
-                    toast.error('Something wrong with site. Contact technical support.', toastParams);
-            }
-        } else {
-            alert('empty');
-        }
-    };
-
     return (
         <div className={styles.wrapper}>
             <h2>Profile</h2>
 
-            <Formik
-                initialValues={{
-                    name: user.displayName || '',
-                    email: user.email || '',
-                    password: '',
-                    confirmPassword: ''
+            <UpdateProfileForm
+                heading="Update Name"
+                fields={['name']}
+                types={['text']}
+                values={{ name: user.displayName }}
+                handleSubmit={({ name }: DynamicObject) => {
+                    if (user.displayName !== name) {
+                        dispatch(updateNameRequest(name));
+                    } else {
+                        toast.error('This name is already in use', toastParams);
+                    }
                 }}
-                validationSchema={validationSchema}
-                onSubmit={() => {}}
-            >
-                {formik => (
-                    <>
-                        <Form
-                            className={styles.form}
-                            onSubmit={e => handleSubmit(e, 'name', formik.values.name)}
-                        >
-                            <h3>Update Name</h3>
-                            <Field type="text" name="name" />
-                            <p>
-                                <ErrorMessage name="name" />
-                            </p>
-                        </Form>
+            />
 
-                        <Form
-                            className={styles.form}
-                            onSubmit={e => handleSubmit(e, 'email', formik.values.email)}
-                        >
-                            <h3>Update Email</h3>
-                            <Field type="email" name="email" />
-                            <p>
-                                <ErrorMessage name="email" />
-                            </p>
-                        </Form>
+            <UpdateProfileForm
+                heading="Update Email"
+                fields={['email']}
+                types={['text']}
+                values={{ email: user.email }}
+                handleSubmit={({ email }: DynamicObject) => {
+                    if (user.email !== email) {
+                        setUpdatingField({ field: 'email', value: email });
+                        setShouldShowModal(true);
+                    } else {
+                        toast.error('This email is already in use', toastParams);
+                    }
+                }}
+            />
 
-                        <Form
-                            className={styles.form}
-                            onKeyDown={e => {
-                                if(e.keyCode === 13) {
-                                    handleSubmit(e, 'password', formik.values.password);
-                                }
-                            }}
-                        >
-                            <h3>Update Password</h3>
-                            <Field type="password" name="password" />
-                            <p>
-                                <ErrorMessage name="password" />
-                            </p>
+            <UpdateProfileForm
+                heading="Update Password"
+                fields={['password', 'confirmPassword']}
+                types={['password', 'password']}
+                values={{ password: '', confirmPassword: '' }}
+                handleSubmit={({ password }: DynamicObject) => {
+                    setUpdatingField({ field: 'password', value: password });
+                    setShouldShowModal(true);
+                }}
+            />
 
-                            <Field type="password" name="confirmPassword" />
-                            <p>
-                                <ErrorMessage name="confirmPassword" />
-                            </p>
-                        </Form>
-                    </>
-                )}
-            </Formik>
+            {shouldShowModal && (
+                <Modal
+                    className={styles.overlayContainer}
+                    handleOverlayClick={() => setShouldShowModal(false)}
+                    handleEscapeButtonClick={() => setShouldShowModal(false)}
+                    onMount={() => ModalInputRef.current?.focus()}
+                >
+                    <form onSubmit={e => reAuthentication(e)}>
+                        <h3>Type your password to confirm changes</h3>
+                        <input
+                            ref={ModalInputRef}
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
 
-            <div className={styles.reAuthForm} style={{ display: reAuthFormDisplay }}>
-                <form onSubmit={e => reAuthentication(e)}>
-                    <h3>Type your password to confirm changes</h3>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-
-                    <div className={styles.buttons}>
-                        <button type="button" onClick={() => setReAuthFormDisplay('none')}>
-                            Cancel
-                        </button>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
+                        <div className={styles.buttons}>
+                            <button type="button" onClick={() => setShouldShowModal(false)}>
+                                Cancel
+                            </button>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                </Modal>
+            )}
         </div>
     );
 }
---
+++ src/components/settingsPart/UpdateProfileForm/UpdateProfileForm.tsx
@@ -0,0 +1,40 @@
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import styles from './UpdateProfileForm.module.css';
+import { DynamicObject } from '../../../types/types';
+import React from 'react';
+
+type Props = {
+    heading: string;
+    values: DynamicObject;
+    fields: string[];
+    types: string[];
+    handleSubmit: Function;
+};
+
+function UpdateProfileForm({ heading, fields, types, values, handleSubmit }: Props) {
+    const validationSchema = Yup.object().shape({ name: Yup.string() });
+
+    return (
+        <Formik
+            initialValues={{ ...values }}
+            validationSchema={validationSchema}
+            onSubmit={values => handleSubmit(values)}
+        >
+            <Form className={styles.form}>
+                <h3>{heading}</h3>
+                {fields.map((field, i) => (
+                    <React.Fragment key={field}>
+                        <Field type={types[i]} name={field} />
+                        <p>
+                            <ErrorMessage name={field} />
+                        </p>
+                    </React.Fragment>
+                ))}
+            </Form>
+        </Formik>
+    );
+}
+
+export default UpdateProfileForm;
